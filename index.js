4ad37357188173243aa17659be73179cafce0dad
Maritaria <>
2020-08-08 19:19:51 +0200

    Implement api to control lamps

--- index.js
+++ index.js
@@ -1,91 +1,52 @@
-import dgram from 'dgram';
-import { Socket } from 'net';
+import MagicHome from 'magic-home';
+import Koa from 'koa';
+import Router from 'koa-router';
+import json from 'koa-json';
+import bodyParser from 'koa-bodyparser';
 
-const PORT = 48899;
-const COMMAND_PORT = 5577;
-const server = dgram.createSocket('udp4');
+const router = new Router();
 
-server.on('error', err => {
-	console.error(`Server error: ${err}`);
-	server.close();
+router.get('/lamps', async (ctx, next) => {
+	const discovery = new MagicHome.Discovery();
+	const devices = await discovery.scan(500);
+	ctx.body = devices;
 });
 
-server.on('listening', () => {
-	const address = server.address();
-	console.log(`Listening ${address.address}:${address.port}`);
+router.get('/lamps/:ip', async (ctx, next) => {
+	const ip = ctx.params.ip;
+	const device = new MagicHome.Control(ip);
+	const state = await device.queryState();
+	ctx.body = state;
 });
 
-server.bind(PORT);
-
-// Device detector
-const broadcastMessage = Buffer.from('HF-A11ASSISTHREAD');
-let broadcastTimer = null;
-
-server.on('listening', () => {
-	server.setBroadcast(true);
-	if (!broadcastTimer) {
-		console.log('Starting broadcast timer');
-		broadcastTimer = setInterval(detectDevices, 5 * 1000);
-		detectDevices();
-		server.on('message', onReply);
-	}
+router.post('/lamps/:ip/power', async (ctx, next) => {
+	const ip = ctx.params.ip;
+	const device = new MagicHome.Control(ip);
+	const input = ctx.request.body;
+	const success = await device.setPower(!!input.power);
+	ctx.body = { success };
 });
 
-server.on('close', () => {
-	if (broadcastTimer) {
-		console.log('Cancelling broadcast timer');
-		cancelInterval(broadcastTimer);
-		broadcastTimer = null;
-	}
+router.post('/lamps/:ip/color', async (ctx, next) => {
+	const ip = ctx.params.ip;
+	const device = new MagicHome.Control(ip);
+	const input = ctx.request.body;
+	const success = await device.setColor(input.red, input.green, input.blue);
+	ctx.body = { success };
 });
 
-function detectDevices() {
-	console.log('Sending detection broadcast...');
-	server.send(broadcastMessage, PORT, '255.255.255.255');
-}
+const app = new Koa();
 
-const devices = new Map();
+// ensureAcceptsJson
+app.use(async (ctx, next) => {
+	ctx.assert(ctx.request.accepts('json'), 406);
+	await next();
+});
 
-function onReply(buffer, source) {
-	if (buffer.equals(broadcastMessage)) return;
-	const message = buffer.toString();
-	const parts = message.split(',');
-	const info = {
-		address: parts[0],
-		id: parts[1],
-		model: parts[2],
-		lastSeen: new Date(),
-	};
-	if (!devices.has(info.id)) {
-		console.log('New device', info);
-		setDeviceLight(info, {});
-	}
-	devices.set(info.id, info);
-}
+app.use(json());
+app.use(bodyParser());
 
-function sendCommand(socket, data) {
-	socket.write(Buffer.from(data));
-}
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-function setDeviceLight(device, color) {
-	const client = new Socket();
-	client.on('data', buffer => {
-		console.log(`Received from ${device.id} ${buffer}`, buffer);
-	});
-	client.on('close', () => {
-		console.log('Disconnected from', device.id);
-	});
-	client.connect(COMMAND_PORT, device.address, () => {
-		console.log('Connected to device', device.id);
-		sendCommand(client, [
-			0xef, 0x01, 0x77
-		]);
-		sendCommand(client, [
-			0x41,
-			// RGB
-			0xff, 0x00, 0x00,
-			// ???
-			0x00, 0x00, 0x0f,
-		]);
-	});
-}
+app.listen(8080);
