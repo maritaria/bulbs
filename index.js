bdf909d09efb1346e3372580383bc634259d71d9
Bram Kamies <bramkamies@gmail.com>
2020-08-08 16:36:06 +0200

    progress

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import dgram from 'dgram';
+import { Socket } from 'net';
 
 const PORT = 48899;
-
+const COMMAND_PORT = 5577;
 const server = dgram.createSocket('udp4');
 
 server.on('error', err => {
 	console.error(`Server error: ${err}`);
 	server.close();
-});
-
-server.on('message', (msg, rinfo) => {
-	console.log(`Received: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
 server.on('listening', () => {
@@ -30,6 +27,7 @@ server.on('listening', () => {
 		console.log('Starting broadcast timer');
 		broadcastTimer = setInterval(detectDevices, 5 * 1000);
 		detectDevices();
+		server.on('message', onReply);
 	}
 });
 
@@ -44,4 +42,50 @@ server.on('close', () => {
 function detectDevices() {
 	console.log('Sending detection broadcast...');
 	server.send(broadcastMessage, PORT, '255.255.255.255');
+}
+
+const devices = new Map();
+
+function onReply(buffer, source) {
+	if (buffer.equals(broadcastMessage)) return;
+	const message = buffer.toString();
+	const parts = message.split(',');
+	const info = {
+		address: parts[0],
+		id: parts[1],
+		model: parts[2],
+		lastSeen: new Date(),
+	};
+	if (!devices.has(info.id)) {
+		console.log('New device', info);
+		setDeviceLight(info, {});
+	}
+	devices.set(info.id, info);
+}
+
+function sendCommand(socket, data) {
+	socket.write(Buffer.from(data));
+}
+
+function setDeviceLight(device, color) {
+	const client = new Socket();
+	client.on('data', buffer => {
+		console.log(`Received from ${device.id} ${buffer}`, buffer);
+	});
+	client.on('close', () => {
+		console.log('Disconnected from', device.id);
+	});
+	client.connect(COMMAND_PORT, device.address, () => {
+		console.log('Connected to device', device.id);
+		sendCommand(client, [
+			0xef, 0x01, 0x77
+		]);
+		sendCommand(client, [
+			0x41,
+			// RGB
+			0xff, 0x00, 0x00,
+			// ???
+			0x00, 0x00, 0x0f,
+		]);
+	});
 }
